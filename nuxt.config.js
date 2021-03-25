b8d32ec67e73694b0d51bf953357fbc44a0b131d
Slava191 <slava191web@gmail.com>
2021-03-25 21:35:36 +0300

    css in config

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/theme.css',
+    '@/assets/fontawesome/js/all.min.js'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
